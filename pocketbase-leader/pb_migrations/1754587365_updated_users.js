737b09aa47c1fb07613130876d1d1c78966565d9
robmolloy <robmolloy@hotmail.co.uk>
2025-08-07 18:29:18 +0100

    correct api rules on rbac and sbac

---
+++ pocketbase-leader/pb_migrations/1754587365_updated_users.js
@@ -0,0 +1,42 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update field
+  collection.fields.addAt(9, new Field({
+    "hidden": false,
+    "id": "select1466534506",
+    "maxSelect": 1,
+    "name": "role",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "select",
+    "values": [
+      "standard",
+      "admin"
+    ]
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update field
+  collection.fields.addAt(9, new Field({
+    "hidden": false,
+    "id": "select1466534506",
+    "maxSelect": 1,
+    "name": "role",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "select",
+    "values": [
+      "standard",
+      "admin"
+    ]
+  }))
+
+  return app.save(collection)
+})
