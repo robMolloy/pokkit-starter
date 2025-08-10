079a250495297bbbf435f7d6dc820de9d1dff502
robmolloy <robmolloy@hotmail.co.uk>
2025-08-10 15:10:21 +0100

    working simplified layouts

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,9 @@
-import { Layout } from "@/components/layout/Layout";
+import { CustomIcon } from "@/components/CustomIcon";
+import { Header } from "@/components/layout/Header";
+import { Button } from "@/components/ui/button";
 import { pb } from "@/config/pocketbaseConfig";
 import { AuthForm } from "@/modules/auth/AuthForm";
+import { logout } from "@/modules/auth/dbAuthUtils";
 import { smartSubscribeToUsers, subscribeToUser } from "@/modules/users/dbUsersUtils";
 import { useUsersStore } from "@/modules/users/usersStore";
 import { AwaitingApprovalScreen } from "@/screens/AwaitingApprovalScreen";
@@ -14,52 +17,164 @@ import {
 import { useThemeStore } from "@/stores/themeStore";
 import "@/styles/globals.css";
 import "@/styles/markdown.css";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@radix-ui/react-tooltip";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useEffect } from "react";
+
+const SidebarButtonWrapper = (p: {
+  children: React.ReactNode;
+  href?: string;
+  disabled?: boolean;
+}) => {
+  return p.href ? (
+    <Link href={p.disabled ? "#" : p.href} className={p.disabled ? "pointer-events-none" : ""}>
+      {p.children}
+    </Link>
+  ) : (
+    p.children
+  );
+};
+
+const PossibleTooltipWrapper = (p: {
+  children: React.ReactNode;
+  tooltipContent?: React.ReactNode;
+}) => {
+  return p.tooltipContent ? (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{p.children}</TooltipTrigger>
+        <TooltipContent>{p.tooltipContent}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  ) : (
+    <>{p.children}</>
+  );
+};
+
+const SidebarButton = (p: {
+  href?: string;
+  iconName?: React.ComponentProps<typeof CustomIcon>["iconName"];
+  children: React.ReactNode;
+  isHighlighted: boolean;
+  onClick?: () => void;
+  badgeCount?: number;
+  disabled?: boolean;
+  tooltipContent?: React.ReactNode;
+}) => {
+  return (
+    <SidebarButtonWrapper href={p.href} disabled={p.disabled}>
+      <PossibleTooltipWrapper tooltipContent={p.tooltipContent}>
+        <Button
+          variant={p.isHighlighted ? "secondary" : "ghost"}
+          className={`w-full ${p.disabled ? "pointer-events-none" : ""}`}
+          onClick={p.onClick}
+          disabled={p.disabled}
+        >
+          <span className="flex w-full items-center gap-2 text-left">
+            {p.iconName && (
+              <CustomIcon
+                iconName={p.iconName}
+                size="sm"
+                className={p.disabled ? "text-muted-foreground" : ""}
+              />
+            )}
+
+            {p.children}
+
+            {p.badgeCount !== undefined && p.badgeCount > 0 && (
+              <span className="absolute right-2 top-1/2 -translate-y-1/2 rounded-full bg-destructive px-2 py-0.5 text-xs text-destructive-foreground">
+                {p.badgeCount}
+              </span>
+            )}
+          </span>
+        </Button>
+      </PossibleTooltipWrapper>
+    </SidebarButtonWrapper>
+  );
+};
 
 const PreserveScroll = (p: {
   children: React.ReactNode;
   className?: HTMLDivElement["className"];
 }) => <div className={`flex h-full flex-col ${p.className ?? ""}`}>{p.children}</div>;
 
-const CustomLeftSidebar = (p: {
+const CustomLeftSidebarTemplate = (p: {
   top: React.ReactNode;
   middle: React.ReactNode;
   bottom: React.ReactNode;
 }) => {
   return (
     <PreserveScroll className="border-r">
-      <div className="border-b p-4">{p.top}</div>
-      <div className="flex-1 overflow-y-auto p-4">{p.middle}</div>
-      <div className="border-t p-4">{p.bottom}</div>
+      <div className="flex flex-col gap-1 border-b p-2">{p.top}</div>
+      <div className="flex flex-1 flex-col gap-1 overflow-y-auto p-2">{p.middle}</div>
+      <div className="flex flex-col gap-1 border-t p-2">{p.bottom}</div>
     </PreserveScroll>
   );
 };
+const CustomLeftSidebar = () => {
+  const router = useRouter();
 
-const CustomLayout = () => {
+  const currentUserStore = useCurrentUserStore();
+  const usersStore = useUsersStore();
+  const pendingUsersCount = usersStore.data.filter((user) => user.status === "pending").length;
+
+  const isAdmin =
+    currentUserStore.data.authStatus === "loggedIn" &&
+    currentUserStore.data.user.status === "approved" &&
+    currentUserStore.data.user.role === "admin";
+  return (
+    <CustomLeftSidebarTemplate
+      top={
+        <SidebarButton href="/" iconName={"Home"} isHighlighted={router.pathname === "/"}>
+          Home
+        </SidebarButton>
+      }
+      middle={
+        <>
+          {[...Array(100)].map((_, j) => (
+            <SidebarButton iconName="Ban" key={j} isHighlighted={j === 2}>
+              do summit {j}
+            </SidebarButton>
+          ))}
+        </>
+      }
+      bottom={
+        <>
+          {isAdmin && (
+            <SidebarButton
+              href="/users"
+              iconName="Users"
+              isHighlighted={router.pathname === "/users"}
+              badgeCount={pendingUsersCount}
+            >
+              Users
+            </SidebarButton>
+          )}
+          <SidebarButton iconName="LogOut" isHighlighted={false} onClick={() => logout({ pb })}>
+            Log Out
+          </SidebarButton>
+        </>
+      }
+    />
+  );
+};
+
+const CustomLayout = (p: { children: React.ReactNode; showLeftSidebar: boolean }) => {
   return (
     <div className="flex h-screen max-h-screen flex-col">
-      <header className="h-16 bg-blue-600 p-4 text-white">
-        <h1 className="text-2xl font-bold">Header Title</h1>
-      </header>
+      <Header />
 
       <div className="flex flex-1 overflow-hidden">
-        <PreserveScroll>
-          <CustomLeftSidebar
-            top={<h2 className="text-lg font-semibold">Fixed Title</h2>}
-            middle={[...Array(100)].map((_, j) => (
-              <div key={j}>{j}</div>
-            ))}
-            bottom={<h2 className="text-lg font-semibold">Fixed Bottom</h2>}
-          />
-        </PreserveScroll>
+        {p.showLeftSidebar && (
+          <PreserveScroll className="w-64">
+            <CustomLeftSidebar />
+          </PreserveScroll>
+        )}
 
-        <div className="flex-1 overflow-y-auto">
-          {[...Array(100)].map((_, j) => (
-            <div key={j}>{j}</div>
-          ))}
-        </div>
+        <div className="flex-1 overflow-y-auto">{p.children}</div>
       </div>
     </div>
   );
@@ -125,14 +240,12 @@ export default function App({ Component, pageProps }: AppProps) {
     onIsLoggedOut: () => {},
   });
 
-  return <CustomLayout />;
-
   return (
     <>
       <Head>
         <title>pokkit Starter</title>
       </Head>
-      <Layout
+      <CustomLayout
         showLeftSidebar={
           currentUserStore.data.authStatus === "loggedIn" &&
           ["approved", "admin"].includes(currentUserStore.data.user.status)
@@ -160,7 +273,44 @@ export default function App({ Component, pageProps }: AppProps) {
 
           return <Component {...pageProps} />;
         })()}
-      </Layout>
+      </CustomLayout>
     </>
   );
+
+  // return (
+  //   <>
+  //     <Head>
+  //       <title>pokkit Starter</title>
+  //     </Head>
+  //     <Layout
+  //       showLeftSidebar={
+  //         currentUserStore.data.authStatus === "loggedIn" &&
+  //         ["approved", "admin"].includes(currentUserStore.data.user.status)
+  //       }
+  //     >
+  //       {(() => {
+  //         if (currentUserStore.data.authStatus === "loading") return <LoadingScreen />;
+
+  //         if (currentUserStore.data.authStatus === "loggedOut")
+  //           return (
+  //             <div className="mt-16 flex justify-center">
+  //               <AuthForm />
+  //             </div>
+  //           );
+
+  //         // should not be required
+  //         if (currentUserStore.data.authStatus !== "loggedIn") {
+  //           console.error(`this line should never be hit`);
+  //           return;
+  //         }
+
+  //         if (currentUserStore.data.user.status === "pending") return <AwaitingApprovalScreen />;
+
+  //         if (currentUserStore.data.user.status === "blocked") return <BlockedScreen />;
+
+  //         return <Component {...pageProps} />;
+  //       })()}
+  //     </Layout>
+  //   </>
+  // );
 }
