1bf36b552c9b1a5a37f43276fb270b1bb9bdbcc4
robmolloy <robmolloy@hotmail.co.uk>
2025-09-25 09:24:10 +0100

    add  back link to otp page

--- src/pages/auth/signin-with-otp.page.tsx
+++ src/pages/auth/signin-with-otp.page.tsx
@@ -3,12 +3,20 @@ import { SimpleCard } from "@/components/uiCustom/SimpleCard";
 import { pb } from "@/config/pocketbaseConfig";
 import { SignInWithOtpForm } from "@/modules/auth/forms/SignInWithOtpForm";
 import { UnauthenticatedOnlyRoute } from "@/modules/auth/routeProtectors/UnauthenticatedOnlyRoute";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <UnauthenticatedOnlyRoute>
       <CenteredItemTemplate>
-        <SimpleCard title="Sign in with OTP">
+        <SimpleCard
+          title="Sign in with OTP"
+          headerChildren={
+            <Link href="/auth" className="text-muted-foreground hover:underline">
+              &lt; Back
+            </Link>
+          }
+        >
           <SignInWithOtpForm pb={pb} />
         </SimpleCard>
       </CenteredItemTemplate>
