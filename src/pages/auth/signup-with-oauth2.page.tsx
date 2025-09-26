1f17730c55c73d5a97f05f181fee9aee043b0fd2
robmolloy <robmolloy@hotmail.co.uk>
2025-09-26 11:43:09 +0100

    signupWithOAuth2Page created

---
+++ src/pages/auth/signup-with-oauth2.page.tsx
@@ -0,0 +1,48 @@
+import { CenteredItemTemplate } from "@/components/layout/CenteredItemTemplate";
+import { Button } from "@/components/ui/button";
+import {
+  FormFeedbackMessages,
+  useFormFeedbackMessages,
+} from "@/components/uiCustom/FormFeedbackMessages";
+import { SimpleCard } from "@/components/uiCustom/SimpleCard";
+import { pb } from "@/config/pocketbaseConfig";
+import { signupWithOAuth2Google } from "@/modules/auth/dbAuthUtils";
+import { UnauthenticatedOnlyRoute } from "@/modules/auth/routeProtectors/UnauthenticatedOnlyRoute";
+import Link from "next/link";
+
+export default function Page() {
+  const formFeedback = useFormFeedbackMessages();
+
+  return (
+    <UnauthenticatedOnlyRoute>
+      <CenteredItemTemplate>
+        <SimpleCard
+          title="Sign in with OTP"
+          headerChildrenBottom={
+            <Link href="/auth" className="text-muted-foreground hover:underline">
+              &lt; Back
+            </Link>
+          }
+        >
+          <div className="flex flex-col gap-4">
+            {formFeedback.messages && formFeedback.status && (
+              <FormFeedbackMessages messages={formFeedback.messages} status={formFeedback.status} />
+            )}
+
+            <Button
+              className="w-full"
+              onClick={async () => {
+                const resp = await signupWithOAuth2Google({ pb });
+
+                const fn = resp.success ? formFeedback.showSuccess : formFeedback.showError;
+                fn(resp.messages);
+              }}
+            >
+              Sign up with Google
+            </Button>
+          </div>
+        </SimpleCard>
+      </CenteredItemTemplate>
+    </UnauthenticatedOnlyRoute>
+  );
+}
