e32aac8f3c910f78d122d00427057b5a1942dd26
robmolloy <robmolloy@hotmail.co.uk>
2025-09-26 12:11:20 +0100

    improve CenteredItemTemplate css

--- src/components/layout/CenteredItemTemplate.tsx
+++ src/components/layout/CenteredItemTemplate.tsx
@@ -1,7 +1,7 @@
 export const CenteredItemTemplate = (p: { children: React.ReactNode }) => {
   return (
-    <div className="mt-16 flex justify-center">
-      <div className="w-[400px]">{p.children}</div>
+    <div className="mt-4 flex justify-center px-4 md:mt-16">
+      <div className="w-[420px] max-w-full">{p.children}</div>
     </div>
   );
 };
